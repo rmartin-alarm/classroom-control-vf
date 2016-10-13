a077fb367de4e8b915f2202f86587566814edac0
rmartin-alarm <rmartin@alarm.com>
2016-10-13 11:51:38 -0400

    Update init.pp
    missing /

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -8,7 +8,7 @@ class memcached {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => 'puppet://modules/memcached/memcached',
+    source  => 'puppet:///modules/memcached/memcached',
     require => Package['memcached'],
   }
   
