e9ce5f69739f217b979feb76146baff2dc251f35
rmartin-alarm <rmartin@alarm.com>
2016-10-13 15:49:22 -0400

    Update init.pp
    code cleanup; leverage the fact that puppet will autoupdate perms with exec bit for traversal

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,10 @@
 class nginx {
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+  
   yumrepo { 'updates':
     ensure     => 'present',
     descr      => 'CentOS-$releasever - Updates',
@@ -13,43 +19,24 @@ class nginx {
     require => Yumrepo['updates'],
   }
   
-  file { '/var/www' :
+  file { ['/var/www','/etc/nginx/conf.d/'] :
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
   }
   
   file { '/var/www/index.html' :
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf' :
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
   
-  file { '/etc/nginx/conf.d/' :
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-  }  
-  
   file { '/etc/nginx/conf.d/default.conf' :
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
